a768486870a91915ab1277147364ae6dd9910a39
thadeus <thadeusnyariki94@gmail.com>
2022-10-25 06:24:46 +0300

    Done assignment

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,17 @@
-require_relative './dance_module'
-require_relative './meta_dancing_module'
+# require_relative './dance_module'
+# require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Dancer
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+end
+
+angelina = Dancer.new
+p angelina.twirl
+# => "I'm twirling!"
+p angelina.jump
+# => "Look how high I'm jumping!"
+
+p Dancer.metadata
+# => "This class produces objects that love to dance."
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,17 @@
-require_relative './dance_module'
-require_relative './meta_dancing_module'
+# require_relative './dance_module'
+# require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Kid
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+end
+
+buster = Kid.new
+p buster.jump
+# => "Look how high I'm jumping!"
+p buster.take_a_bow
+# => "Thank you, thank you. It was a pleasure to dance for you all."
+
+p Kid.metadata
+# => "This class produces objects that love to dance."

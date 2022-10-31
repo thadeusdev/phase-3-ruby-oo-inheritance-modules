07d7ad20ef258393338287dbc38c0666e352a03b
thadeusdev <109853680+thadeusdev@users.noreply.github.com>
2022-10-31 10:22:30 +0300

    Update dancer.rb

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,17 +1,15 @@
-# require_relative './dance_module'
-# require_relative './meta_dancing_module'
-require_relative './fancy_dance'
+require_relative './dance_module'
+require_relative './meta_dancing_module'
+require_relative './fancy_dance.rb'
 
 class Dancer
     extend FancyDance::ClassMethods
     include FancyDance::InstanceMethods
+
+attr_accessor :name
+
+def initialize (name)
+    @name = name
 end
 
-angelina = Dancer.new
-p angelina.twirl
-# => "I'm twirling!"
-p angelina.jump
-# => "Look how high I'm jumping!"
-
-p Dancer.metadata
-# => "This class produces objects that love to dance."
+end
